5b45eb13e07fffad1186e1ac5dfeb49b365dbd99
sir-da-silva <cirildasilva1@gmail.com>
2024-01-16 10:39:42 -0500

    add popup confirm

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
 import { createRoot } from "react-dom/client";
 // my modules
 import { ImageUploader } from "./image-uploader";
+import { Confirm } from "./confirm";
 
 const root = createRoot(document.getElementById('container'))
 root.render(<ImageUploader/>)
+
+Confirm(
+    'fa-triangle-exclamation', // icon
+    'Action sensible', // title
+    'Voulez vous vraiment continuer cet action ?', // text
+    'Annuler', // button 1
+    null, // function 1
+    'Continuer', // button 2
+    () => {alert('Vous avez continué l\'action')} // function 2
+)
